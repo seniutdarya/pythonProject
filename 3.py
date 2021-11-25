335d8d2e9250a74427643bd1a826726065ce1768
Vasili Seniuts <vasian_seniut@mail.ru>
2021-11-26 00:21:38 +0300

    Initial commit

---
+++ 3.py
@@ -0,0 +1,22 @@
+# Найдите сумму номеров минимального и максимального элементов.
+#a = [5, 6, 6, 8, 4, 9, 2, 8]
+
+
+#b = a.index(max(a))
+#c = a.index(min(a))
+
+#m = b+c
+#print(m)
+
+def dog_age2human_age(dog_age):
+    """dog_age2human_age(dog_age)"""
+    if dog_age == 1:
+        human_age = 14
+    elif dog_age == 2:
+        human_age = 22
+    else:
+        human_age =  22 + (dog_age-2)*5
+    return human_age
+
+age = int(input("How old is your dog? "))
+print(f"This corresponds to {dog_age2human_age(age)} human years!")
